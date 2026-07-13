d5f0f7a478f539f036f1b6024777a3eb0c0abf6a
Aaron Kim <aaron.kim@tdk.com>
2026-07-13 14:34:42 +0900

    Added the header file from MakeLists in driver to avoid sensor all test suite

--- icm566xx/imu/inv_imu_defs.h
+++ icm566xx/imu/inv_imu_defs.h
@@ -20,6 +20,9 @@ extern "C" {
 
 #include <stdint.h>
 
+/* Include device definition */
+#include "icm566xx/icm566xx_h/imu/inv_imu.h"
+
 /* Whoami */
 #define INV_IMU_FSYNC_SUPPORTED    1
 #define INV_IMU_INT2_PIN_SUPPORTED 1
